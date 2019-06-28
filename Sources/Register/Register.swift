d938d43ac62905906d74cc98729052ed4cca3b2d
Chris Eidhof <chris@eidhof.nl>
2019-06-28 11:31:07 +0200

    Collection conformance

--- Sources/Register/Register.swift
+++ Sources/Register/Register.swift
@@ -3,12 +3,27 @@ public struct Token: Hashable, Equatable {
     init(_ i: Int) { self.i = i }
 }
 
-public struct Register<A> {
-    private var items: [Token:A] = [:]
+public struct Register<A>: Collection {
+    public typealias Items = [Token:A]
+    private var items: Items = [:]
     private let freshNumber: () -> Token
     public init() {
         var iterator = (Int.min...).makeIterator()
         freshNumber = { Token(iterator.next()!) }
+    }
+    
+    public var startIndex: Items.Index {
+        return items.startIndex
+    }
+    public var endIndex: Items.Index {
+        return items.endIndex
+    }
+    public func index(after i: Dictionary<Token, A>.Index) -> Dictionary<Token, A>.Index {
+        return items.index(after: i)
+    }
+    
+    public subscript(index: Items.Index) -> Items.Element {
+        return items[index]
     }
 
     @discardableResult
